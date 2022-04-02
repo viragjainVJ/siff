4f30fa8723bda1946a4ca982c56bd19406ae65e7
Virag Jain <virag.j.jain@oracle.com>
2022-04-02 14:01:48 +0530

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../css/main.css';
 import WorkProgress from './common/WorkProgress';
+import Whatsapp from '../images/whatsapp.png';
 
 const Home = () => {
     return (
         <div className='content-wrap'>
-            <h1 className='helpline-header'>Home</h1>
+            {/* <h1 className='helpline-header'>Home</h1> */}
             <div className='faq-page'>
+                <div className='seek-help'>
+                    <p>To  seek  free  help and support from  SIFF Counselors  &  to  talk to  our  Media Spokespersons, please click the <a href='https://chat.whatsapp.com/LaVQYNAjPI8Cg2KRvbUjZh'><img className='whatsapp' src={Whatsapp} alt="Whatsapp"></img> WhatsApp Link</a> here. </p>
+                    <p>SIFF काउंसलर्स से मुफ्त में मदद लेने के लिए और हमारे मीडिया प्रवक्ताओं से बात करने के लिए, कृपया यहाँ <a href='https://chat.whatsapp.com/LaVQYNAjPI8Cg2KRvbUjZh'><img className='whatsapp' src={Whatsapp} alt="Whatsapp"></img> व्हाट्सएप लिंक</a> पर क्लिक करें।</p>
+                </div>
                 <WorkProgress />
             </div>
         </div>
--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,6 +6,7 @@ import Facebook from '../images/facebook.png';
 import Twitter from '../images/twitter.png';
 import YouTube from '../images/youtube.png';
 import Telegram from '../images/telegram.png';
+import WhatsApp from '../images/whatsapp.png';
 
 const Footer = () => {
     const MenuItem = () => {
@@ -44,7 +45,13 @@ const Footer = () => {
             "url": "https://t.me/joinchat/VEHivEmE-66cyKVs",
             "alt": "Telegram",
             "thumbnail": Telegram
-        }
+        },
+        // {
+        //     "platform": "WhatsApp",
+        //     "url": "https://chat.whatsapp.com/LaVQYNAjPI8Cg2KRvbUjZh",
+        //     "alt": "WhatsApp",
+        //     "thumbnail": WhatsApp
+        // }
     ]
 
     const SocialMedias = () => {
